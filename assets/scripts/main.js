d8c7f1b38d341b5774d2745baea2f587e216602a
Son Hong Su <son.ho463@gmail.com>
2022-07-05 19:49:20 +0900

    アニメーション

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -56,3 +56,39 @@ window.addEventListener("scroll", () => {
     removeClassAction();
   }
 });
+
+// Swiper.js
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	speed: 1000,
+	autoplay: true,
+	centeredSlides: true,
+	effect: "fade",
+	breakpoints: {
+		768: {
+			slidesPerView: 3.5,
+			// spaceBetween: 020
+		},
+	},
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: ".swiper-scrollbar",
+		draggable: true,
+	},
+});
