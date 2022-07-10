87eb7bf895b0645b0182550c8b35602e1b7c6eb1
Son Hong Su <son.ho463@gmail.com>
2022-07-10 21:53:40 +0900

    PC SPOK

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,105 +8,82 @@ const gNavTextJa = document.querySelectorAll(".l-header__gnav__text-ja");
 const gNavTextEn = document.querySelectorAll(".l-header__gnav__text-en");
 const gNavText = [gNavTextEn, gNavTextJa];
 const mv = document.querySelector(".js-mv");
-const gNav = document.querySelector(".l-header__gnav");
+const gNav = document.querySelector(".l-header__gnav__wrapper");
+const headerClone = document.querySelector(".l-header--clone");
 
 // ヘッダーが現れるときのアクション
-const addClassAction = () => {
-  // header.classList.add("fixed");
-  headerLogo.classList.add("display-none");
-  headerLogoBlack.classList.add("display-block");
-  hamburgerLines.forEach((line) => {
-    line.classList.add("hamburger__line--black");
-  });
-  gNavText.forEach((ele) => {
-    ele.forEach((ele) => {
-      ele.classList.add("js-color-black");
-    });
-  });
-  mv.classList.add("top-0");
+const options = {
+  rootMargin: "80%",
+  threshold: 1.0,
 };
-
-// ヘッダーが消えるときのアクション
-const removeClassAction = () => {
-  gNavText.forEach((ele) => {
-    // header.classList.remove("fixed");
-    headerLogo.classList.remove("display-none");
-    headerLogoBlack.classList.remove("display-block");
-    hamburgerLines.forEach((line) => {
-      line.classList.remove("hamburger__line--black");
-    });
-    ele.forEach((ele) => {
-      ele.classList.remove("js-color-black");
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((e) => {
+    if (!e.isIntersecting) {
+      headerClone.classList.add("clone-open");
+    }
+    if (e.isIntersecting) {
+      headerClone.classList.remove("clone-open");
+    }
   });
-  mv.classList.remove("top-0");
-};
+}, options);
+observer.observe(mv);
 
-window.addEventListener("scroll", () => {
-  const offsetY = window.scrollY;
-  console.log(offsetY);
-  if (offsetY > 120) {
-    addClassAction();
-  }
-  if (offsetY > 400) {
-    header.classList.add("appear");
-  }
-  if (offsetY < 400) {
-    header.classList.remove("appear");
-  }
-  if (offsetY < 70) {
-    removeClassAction();
-  }
-});
 
 // ハンバーガーメニューオープン
 const hamOpenAction = () => {
   document.body.style.overflow = "hidden";
-  gNav.classList.add("ham-open");
-  gNavTextJa.forEach((ele) => {
-    ele.classList.remove("js-color-black");
-  });
-  gNavTextEn.forEach((ele) => {
-    ele.classList.remove("js-color-black");
-  });
-  headerLogo.classList.remove("display-none");
-  headerLogoBlack.classList.remove("display-block");
-  hamburger.classList.add("hamburger__open");
-  hamburgerLines.forEach((line) => {
-    line.classList.remove("hamburger__line--black");
-  });
+  header.classList.add("l-header--open");
 };
 const hamCloseAction = () => {
   document.body.style.overflow = "auto";
-
-  gNav.classList.remove("ham-open");
-  const offsetY = window.scrollY;
-  if (offsetY !== 0) {gNavTextJa.forEach((ele) => {
-			ele.classList.add("js-color-black");
-		});
-		gNavTextEn
-		.forEach((ele) => {
-			ele.classList.add("js-color-black");
-		});
-    headerLogo.classList.add("display-none");
-    headerLogoBlack.classList.add("display-block");
-    hamburgerLines.forEach((line) => {
-      line.classList.add("hamburger__line--black");
-    });
-  }
-  hamburger.classList.remove("hamburger__open");
+  header.classList.remove("l-header--open");
 };
 
 const hamOpen = () => {
-  if (gNav.classList.contains("ham-open")) {
+  if (header.classList.contains("l-header--open")) {
     hamCloseAction();
   } else {
     hamOpenAction();
   }
 };
 
-// loading-animation
+// クローンメニューを開いてスクロール止める
+const cloneMenuOpenAction = () => {
+  headerClone.classList.add("clone-menu-open");
+  document.body.style.overflow = "hidden";
+};
+const cloneMenuCloseAction = () => {
+  headerClone.classList.remove("clone-menu-open");
+  document.body.style.overflow = "auto";
+};
 
+const cloneMenuOpen = () => {
+  if (headerClone.classList.contains("clone-menu-open")) {
+    cloneMenuCloseAction();
+  } else {
+    cloneMenuOpenAction();
+  }
+};
+
+// 画面リサイズ監視 スマホハンバーガーメニューが開いていたら閉じてからPCで画面へ
+
+const ro = new ResizeObserver((entries) => {
+  for (let entry of entries) {
+    let w = entry.contentRect.width;
+    console.log(w);
+    if (w > 1028) {
+			if(headerClone.classList.contains('clone-menu-open')){
+				cloneMenuCloseAction();
+			}
+			if(header.classList.contains('l-header--open')){
+				hamCloseAction();
+			}
+    }
+  }
+});
+ro.observe(document.body);
+
+// loading-animation
 window.onload = function () {
   const loadingAnimation = document.querySelector(".c-loading-animation");
   loadingAnimation.classList.add("loaded");
